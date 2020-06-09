9a2b56840b35126f4fbb70220efc96ce24013adc
Hel <helenahrubesova80@gmail.com>
2020-06-08 17:48:28 -0700

    new user added into the database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,28 @@ const config = {
     messagingSenderId: "570323104816",
     appId: "1:570323104816:web:d2ae6deffbf81ddb2569f5",
     measurementId: "G-4KYY909220"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapShot = await userRef.get();
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth; 
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  return userRef;
 }
 
 firebase.initializeApp(config);
